eab7d84b0a3822e70cf4c0350c908492b126872a
ArkadiuszMajewski <119493816+ArkadiuszMajewski@users.noreply.github.com>
2023-08-09 21:39:42 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import Filter from './Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: JSON.parse(localStorage.getItem('contact')) ?? [],
     filter: '',
   };
   addContact = ({ name, number }) => {
@@ -51,25 +46,29 @@ class App extends Component {
   };
 
   filteredContacts = () => {
-    if (this.state.contacts.length > 0) {
-      return this.state.contacts
-        .map(
-          contact =>
-            contact.name
-              .toLowerCase()
-              .includes(this.state.filter.toLowerCase()) && contact
-        )
-        .filter(contact => contact !== false);
-    }
+    return this.state.contacts
+      .map(
+        contact =>
+          contact.name
+            .toLowerCase()
+            .includes(this.state.filter.toLowerCase()) && contact
+      )
+      .filter(contact => contact !== false);
   };
 
   componentDidUpdate() {
     console.log('sadasds');
     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
   }
+  componentDidUpdate(prevState) {
+    console.log(prevState);
+    prevState.contacts !== this.state.contacts &&
+      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
+  }
+
   componentDidMount() {
-    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    const localStorageElem = localStorage.getItem('contacts');
+    localStorage.setItem('contact', JSON.stringify(this.state.contacts));
+    const localStorageElem = localStorage.getItem('contact');
     console.log(localStorageElem);
     this.setState(prevState => ({
       ...prevState,
