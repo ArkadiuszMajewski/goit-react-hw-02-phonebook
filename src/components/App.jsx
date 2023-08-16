5474d5f01d20c430c92fb095307cb40a97eab9fa
ArkadiuszMajewski <119493816+ArkadiuszMajewski@users.noreply.github.com>
2023-08-16 11:33:26 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,10 @@ import Filter from './Filter/Filter';
 class App extends Component {
   state = {
     contacts: [
-      { name: 'Rosie Simpson', number: '459-12-56' },
-      { name: 'Hermione Kline', number: '443-89-12' },
-      { name: 'Eden Clements', number: '645-17-79' },
-      { name: 'Annie Copeland', number: '227-91-26' },
+      // { name: 'Rosie Simpson', number: '459-12-56' },
+      // { name: 'Hermione Kline', number: '443-89-12' },
+      // { name: 'Eden Clements', number: '645-17-79' },
+      // { name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -96,14 +96,15 @@ class App extends Component {
         <ContactForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
         <Filter value={this.state.filter} onChange={this.filterOnChange} />
-        {newContacts.length > 0 ? (
-          <ContactList
-            contacts={newContacts}
-            deleteContact={this.deleteContact}
-          />
-        ) : (
-          <p>No contacts</p>
-        )}
+        {
+          // newContacts.length > 0 ?
+          (
+            <ContactList
+              contacts={newContacts}
+              deleteContact={this.deleteContact}
+            />
+          ) || <p>No contacts</p>
+        }
       </div>
     );
   }
